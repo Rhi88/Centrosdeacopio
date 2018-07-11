9dcaae7209e57898c484c3c7500daf495528a815
Rhi88 <rhizdo_nop@hotmail.com>
2018-07-11 13:20:49 -0500

    Page pilas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PilasComponent } from './pilas/pilas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PilasComponent
   ],
   imports: [
     BrowserModule
