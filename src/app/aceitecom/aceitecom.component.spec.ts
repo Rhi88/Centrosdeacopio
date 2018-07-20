cf35e6489f0ff0869e49ee37ecc000114eb24ab7
Rhi88 <rhizdo_nop@hotmail.com>
2018-07-20 03:36:36 -0500

    Ahora todo tiene routing

--- src/app/aceitecom/aceitecom.component.spec.ts
+++ src/app/aceitecom/aceitecom.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavComponent } from './nav.component';
+import { AceitecomComponent } from './aceitecom.component';
 
-describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+describe('AceitecomComponent', () => {
+  let component: AceitecomComponent;
+  let fixture: ComponentFixture<AceitecomComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ AceitecomComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavComponent);
+    fixture = TestBed.createComponent(AceitecomComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { PilasComponent } from './pilas/pilas.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AceitecomComponent } from './aceitecom/aceitecom.component';
+import { AceiteautoComponent } from './aceiteauto/aceiteauto.component';
+import { ElectronicosComponent } from './electronicos/electronicos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    PilasComponent,
+    AceitecomComponent,
+    AceiteautoComponent,
+    ElectronicosComponent,
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
