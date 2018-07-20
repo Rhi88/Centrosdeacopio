8b3e8a0f9bbc1ad1478c990637c78dcc3ec3f453
Rhi88 <rhizdo_nop@hotmail.com>
2018-07-20 15:33:25 -0500

    Fixed issue with navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { PilasComponent } from './pilas/pilas.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AceitecomComponent } from './aceitecom/aceitecom.component';
@@ -13,7 +12,7 @@ import { ElectronicosComponent } from './electronicos/electronicos.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
+    
     PilasComponent,
     AceitecomComponent,
     AceiteautoComponent,
